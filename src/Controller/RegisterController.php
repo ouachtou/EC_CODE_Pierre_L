12f3dc00e2a9b7484bd371749d1a20cbc018887d
Pierre LEGRAND <p.legrand@kissgroup.io>
2025-01-09 23:18:01 +0100

    booksRead + booksReading

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -10,31 +10,33 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'auth.register')]
     public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        
         $user = new User();
         $form = $this->createForm(RegisterFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $Password = $form->get('password')->getData();
-            $hashedPassword = $passwordHasher->hashPassword($user, $Password);
-            $user->setEmail($form->get('email')->getData());
-            $user->setPassword($hashedPassword);
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+            $this->handleUserRegistration($form, $user, $passwordHasher, $entityManager);
             return $this->redirectToRoute('auth.login');
         }
-
 
         return $this->render('auth/register.html.twig', [
             'RegisterForm' => $form->createView(),
         ]);
+    }
+
+    private function handleUserRegistration($form, User $user, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): void
+    {
+        $hashedPassword = $passwordHasher->hashPassword($user, $form->get('password')->getData());
+        $user->setEmail($form->get('email')->getData());
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
     }
 }
